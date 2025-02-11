b19317934813a0ad1b213f984a4fafd58e1b2c78
Ahmed Ifham <ifham.maa@gmail.com>
2025-02-11 12:20:25 +0530

    Added the Challnges in each component folders and made them have there own component space

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from 'react'
-import Header from './components/Header/Header'
-import Footer from './components/Footer/Footer'
-import MainContent from './components/MainContent/MainContent'
+import Header from './components/Challenge01/Header/Header'
+import Footer from './components/Challenge01/Footer/Footer'
+import MainContent from './components/Challenge01/MainContent/MainContent'
+import JSXRules from './components/Challenge02/JSXRules'
 
 //function will start as function App(){ return (<h1>Hi</h1>) }
 //Arrow function as below
 const App = () => {
   return (
     <>
-      <Header />
+      {/* Challenge 01 */}
+      {/* <Header />
       <MainContent />
-      <Footer />
+      <Footer /> */}
+
+
+      {/* Challenge 02 */}
+      <JSXRules />
+
     </>
   )
 }
---
+++ src/components/Challenge02/JSXRules.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const JSXRules = () => {
+    return(
+        <>
+            <div>
+                <h1>JSX Rules</h1>
+                <p>
+                    <ul>
+                        <li>JSX must return a <b>Single parent element</b></li>
+                        <li>JSX elements must be <b>properly closed</b></li>
+                        <li>JSX attributes are written using <b>camelCase</b>(eg: className instead of class)</li>
+                    </ul>
+                </p>
+            </div>
+        </>
+    )
+}
+
+export default JSXRules
